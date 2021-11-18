76695b997d7128f1dc4bc9197dbe7e02a85305c1
Ann Leonard <annleonard@Anns-MacBook-Air.local>
2021-11-18 14:38:54 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,9 @@ function buildJumbo2(){
     let secondJumbo = $('<div id="secondJumbo" data-toggle="on" class="animated slow fadeInDown delay-1s jumbotron bg-dark text-light mx-auto text-center rounded">')
     let jumboHead = $('<h1 class="pb-4">Skills and Technologies</h1>')
     let jumboList = $('<div class="list-group list-group-flush">')
+    let jsIcon = $('<div class="list-group-item-secondary pb-2"><i class="devicon-javascript-plain colored">Javascript</i>')
     let nodeIcon = $('<div class="list-group-item-secondary pb-2"><i class="devicon-nodejs-plain colored">Node.js</i>')
+    let javaIcon = $('<div class="list-group-item-secondary pb-2"><i class="devicon-java-plain colored">Java</i>')
     let gitIcon = $('<div class="list-group-item-secondary pb-2"><i class="devicon-git-plain colored">Git</i>')
     let linuxIcon = $('<div class="list-group-item-secondary pb-2"><i class="devicon-linux-plain colored">Linux</i>')
     let expressIcon = $('<div class="list-group-item-secondary pb-2"><i style="color:whitesmoke" class="devicon-express-original colored">Express.js</i>')
@@ -17,7 +19,9 @@ function buildJumbo2(){
     let nextBtn = $('<button id="next2" class=" animated fadeIn slow delay-2s m-2 btn btn-lg btn-primary rounded-pill">▼</button>')         
     secondJumbo.append(jumboHead)
     jumboHead.append(jumboList)
+    jumboList.append(jsIcon)
     jumboList.append(nodeIcon)
+    jumboList.append(javaIcon)
     jumboList.append(gitIcon)
     jumboList.append(linuxIcon)
     jumboList.append(expressIcon)
@@ -26,7 +30,7 @@ function buildJumbo2(){
     jumboList.append(mongoIcon)
     jumboList.append(herokuIcon)
     jumboList.append(awsIcon)
-    secondJumbo.append(nextBtn)
+    // secondJumbo.append(nextBtn)
     jumboDiv.append(secondJumbo)
     $("#next2").click(()=>{
         $("#next2").removeClass('animated fadeIn slow')
@@ -43,6 +47,22 @@ $("#profPic").hover(function(){
 })
 
 
+// function buildJumbo3(){
+//     let thirdJumbo = $('<div id="thirdJumbo" class="animated slow fadeInDown delay-1s jumbotron bg-dark text-light mx-auto text-center rounded">')
+//     let jumboHead = $('<h1 class="pb-4">Projects</h1>')
+//     let jumboList = $('<div class="list-group list-group-secondary">')
+//     let firstProd= $('<div class="d-flex w-100 justify-content-between text-center list-group-item"><h3 class="mb-1"><a href="http://ibetya.io">iBetya</a></h3></div><p class="mb-1 prod">iBetya is a collabrative project with an Express server, Passport.js for authentication, a mysql database, and a Bootstrap frontend, hosted on Heroku.</p></a>')
+//     let secondProd= $('<div class="d-flex w-100 justify-content-between text-center list-group-item"><h3 class="mb-1"><a href="http://github.com/ann-leonard/liri-node-app">Liri.js</a></h3></div><p class="mb-1 prod">Liri is a simple command line app using axios to make restful API calls for movie, song, and concert information.</p></a>')
+//     let moreProd = $('<p><a href="http://github.com/ann-leonard">See more projects on my github</p>')
+    
+//     thirdJumbo.append(jumboHead)
+//     thirdJumbo.append(jumboList)
+//     jumboList.append(firstProd)
+//     jumboList.append(secondProd)
+//     thirdJumbo.append(moreProd)
+//     jumboDiv.append(thirdJumbo)
+// }
+
 function jumboDisplay(){
     console.log('gothere')
     console.log(current)
@@ -51,10 +71,10 @@ function jumboDisplay(){
         current = 'jumbo2'
         console.log('jum2')
     }
-    else if (current === 'jumbo2'){
-        buildJumbo3()  
-        console.log("jum3") 
-    }
+    // else if (current === 'jumbo2'){
+    //     buildJumbo3()  
+    //     console.log("jum3") 
+    // }
 }
 
 $("#next").click(()=>{
@@ -65,19 +85,3 @@ $("#next").click(()=>{
     jumboDisplay()
 })
 
-
-function buildJumbo3(){
-    let thirdJumbo = $('<div id="thirdJumbo" class="animated slow fadeInDown delay-1s jumbotron bg-dark text-light mx-auto text-center rounded">')
-    let jumboHead = $('<h1 class="pb-4">Projects</h1>')
-    let jumboList = $('<div class="list-group list-group-secondary">')
-    let firstProd= $('<div class="d-flex w-100 justify-content-between text-center list-group-item"><h3 class="mb-1"><a href="http://ibetya.io">iBetya</a></h3></div><p class="mb-1 prod">iBetya is a collabrative project with an Express server, Passport.js for authentication, a mysql database, and a Bootstrap frontend, hosted on Heroku.</p></a>')
-    let secondProd= $('<div class="d-flex w-100 justify-content-between text-center list-group-item"><h3 class="mb-1"><a href="http://github.com/ann-leonard/liri-node-app">Liri.js</a></h3></div><p class="mb-1 prod">Liri is a simple command line app using axios to make restful API calls for movie, song, and concert information.</p></a>')
-    let moreProd = $('<p><a href="http://github.com/ann-leonard">See more projects on my github</p>')
-    
-    thirdJumbo.append(jumboHead)
-    thirdJumbo.append(jumboList)
-    jumboList.append(firstProd)
-    jumboList.append(secondProd)
-    thirdJumbo.append(moreProd)
-    jumboDiv.append(thirdJumbo)
-}
